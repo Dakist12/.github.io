4a83ef7ad0319e75045a3a58de50b15f4d283163
Dakist12 <162664639+Dakist12@users.noreply.github.com>
2024-09-08 19:10:38 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
+const circle = document.getElementById('circle');
+
+circle.addEventListener('click', function() {
+    if (circle.textContent === 'Offline') {
+        circle.textContent = 'Online';
+        circle.style.backgroundColor = 'green';
+    } else {
+        circle.textContent = 'Offline';
+        circle.style.backgroundColor = 'red';
+    }
+});
 
